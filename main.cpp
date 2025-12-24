d68a458facbac2be01349fbbc7adeff98706b867
Daniel Oliveira <136137847+D4nielStone@users.noreply.github.com>
2025-12-24 20:41:27 -0300

    Trying layout

--- main.cpp
+++ main.cpp
@@ -10,8 +10,19 @@ int main() {
     bgui::set_up();
 
     // The main page
-    bgui::get_layout().add<bgui::text>("Todo List", 0.5f);
-    bgui::apply_style(bgui::dark_style);
+    auto& top_div = bgui::get_layout().add<bgui::linear>(bgui::orientation::vertical);
+    top_div.request_width(bgui::mode::match_parent);
+    top_div.request_height(bgui::mode::pixel, 100.f);
+    top_div.set_alignment(bgui::alignment::center);
+    top_div.set_cross_alignment(bgui::alignment::center);
+    bgui::ft_load_system_font("Noto Serif-Medium");
+    top_div.add<bgui::text>("Todo List", 0.5f).set_font("Noto Serif-Medium");
+    top_div.add<bgui::input_area>("", 0.5f, "Task Title").request_width(bgui::mode::match_parent);
+    top_div.set_visible(true);
+    auto new_style = bgui::light_style;
+    new_style.m_text_color = {1, 1, 1, 1};
+    bgui::apply_style(new_style);
+    top_div.get_material().set("bg_color", bgui::color{0.5, 0.1, 0.1, 1});
 
     while(!bgui::should_close_glfw()) {
         // update the context with glfw
