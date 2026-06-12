fe04072af012220e2bdb7f11bc88dfcff27a70b7
D4nielStone <do1167475@gmail.com>
2026-06-12 19:47:38 -0300

    Refactor backend: Simplify package.json and implement basic server functionality
    - Updated package.json to remove unused dependencies and added start script.
    - Created a basic Express server in server.js with a root endpoint and a user registration endpoint.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,29 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'API online'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000.')
+})
+
+const users =  [];
+
+app.post('/register', (req, res) => {
+    const { email, password } = req.body
+    users.push({
+        email,
+        password
+    })
+    
+    res.json({
+        success: true
+    })
+})
